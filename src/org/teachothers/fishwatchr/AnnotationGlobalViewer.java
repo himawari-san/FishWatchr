d6ef2014fd4bb7e013c301d44bf5bdbb88e54727
himawari-san <himawari@ninjal.ac.jp>
2015-05-31 22:37:34 +0900

    refine the global viewer 02

--- src/org/teachothers/fishwatchr/AnnotationGlobalViewer.java
+++ src/org/teachothers/fishwatchr/AnnotationGlobalViewer.java
@@ -3,44 +3,43 @@ package org.teachothers.fishwatchr;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 
-import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.border.EtchedBorder;
 
 public class AnnotationGlobalViewer extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private CommentList commentList;
-	private int x0 = 0;
-	private int y0 = 0;
-//	private HashMap<String, Integer> keys = new HashMap<String, Integer>();
-//	private int mediaLength = 0;
+	private final int x0NamePanel = 4; // x origin of namePanel
+	private final int y0NamePanel = 4; // y origin of namePanel
+	private final int widthNamePanel = 50;
 
-	private int markWidth = 3;
+	private final int x0AnnotationViewerPanel = 2;
+	private final int y0AnnotationViewerPanel = 4;	
+	
+	private CommentList commentList;
+
+	private int itemHeight = 25;
+	
+	private int markWidth = 2;
 	private int markHeight = 20;
 	
-	private int xMax = 0;
+	private int scaleFactor = 4; // x scaleFactor
 	
-	private boolean flag = false;
 	private int currentTime = 0;
 
 	private JPanel namePanel;
 	private JPanel displayPanel;
 	private JPanel annotationViewerPanel;
-	private JProgressBar timeBar;
+	private JComboBox<String> targetSelector;
+	private String[] targets = {"話者", "ラベル", "注釈者"};
 	
 	private ArrayList<User> discussers;
 	private ArrayList<CommentType> commentTypes;
@@ -53,7 +52,6 @@ public class AnnotationGlobalViewer extends JPanel {
 	public AnnotationGlobalViewer(CommentList commentList, SoundPlayer soundPlayer, ArrayList<User> discussers, ArrayList<CommentType> commentTypes) {
 //		super();
 		this.commentList = commentList;
-//		this.mediaLength = mediaLength;
 		this.discussers = discussers;
 		this.commentTypes = commentTypes;
 		this.soundPlayer = soundPlayer;
@@ -68,25 +66,24 @@ public class AnnotationGlobalViewer extends JPanel {
 	
 
 	private void ginit(){
-		x0 = 0;
-		y0 = this.getSize().height;
-
 		setLayout(new BorderLayout());
 		JPanel p1 = new JPanel();
 		p1.setLayout(new BorderLayout());
 		JPanel p2 = new JPanel();
-		p2.add(new JButton("test"));
+		p2.setLayout(new FlowLayout(FlowLayout.LEFT));
 		namePanel = getNamePanel();
-		namePanel.setPreferredSize(new Dimension(50, Short.MAX_VALUE));
+		namePanel.setPreferredSize(new Dimension(widthNamePanel, Short.MAX_VALUE));
 		namePanel.setBorder(new EtchedBorder());
 		displayPanel = new JPanel();
 		displayPanel.setLayout(new BorderLayout());
 		annotationViewerPanel = getAnnotationViewerPanel();
 		annotationViewerPanel.setBorder(new EtchedBorder());
-		timeBar = new JProgressBar();
 		
+		targetSelector = new JComboBox<String>(targets);
+		p2.add(targetSelector);
+
 		displayPanel.add(annotationViewerPanel, BorderLayout.CENTER);
-		displayPanel.add(timeBar, BorderLayout.SOUTH);
+//		displayPanel.add(timeBar, BorderLayout.SOUTH);
 		
 		p1.add(namePanel, BorderLayout.LINE_START);
 		p1.add(displayPanel, BorderLayout.CENTER);
@@ -103,47 +100,46 @@ public class AnnotationGlobalViewer extends JPanel {
 				@Override
 				protected void paintComponent(Graphics g) {
 					super.paintComponent(g);
-					int max = 0;
-					xMax = getSize().width;
+					int x;
+					String discusserName;
+					String commenterName;
+					String commentType;
 					
 					for(Comment comment : commentList){
-						int time = commentList.unifiedCommentTime(comment) / 1000;
-						if(max < time){
-							max = time;
-						}
-					}
-					
-					
-					int n = 0;
-					for(Comment comment : commentList){
-						int time = commentList.unifiedCommentTime(comment) / 1000;
-						String discusserName = comment.getDiscusser().getName();
-						String commenterName = comment.getCommenter().getName();
-					
-						int x = time/4;
-						int y0 = 100;
-						if(x > xMax){
-							x -= xMax;
-							y0 = 250;
-						}
+						x = x0AnnotationViewerPanel +
+								commentList.unifiedCommentTime(comment) / 1000 / scaleFactor;
 						g.setColor(comment.getCommentType().getColor());
-						g.fillRect(x, discusserNames.indexOf(discusserName)*25 , 2, 20);
-//						System.err.println(discusserName + ":" + discusserNames.indexOf(discusserName));
+
+						switch (targetSelector.getSelectedIndex()){
+						case 0:
+							discusserName = comment.getDiscusser().getName();
+							g.fillRect(x, y0AnnotationViewerPanel + discusserNames.indexOf(discusserName)*itemHeight , markWidth, markHeight);
+							break;
+						case 1:
+							commentType = comment.getCommentType().getType();
+							g.fillRect(x, y0AnnotationViewerPanel + types.indexOf(commentType)*itemHeight , markWidth, markHeight);
+							break;
+						case 2:
+							commenterName = comment.getCommenter().getName();
+							g.fillRect(x, y0AnnotationViewerPanel + commenterNames.indexOf(commenterName)*itemHeight , markWidth, markHeight);
+							break;
+						}
 					}
-					int x1 = soundPlayer.getElapsedTime()/4000;
-					g.drawLine(x1, 0, x1, 300);
+					int xTime = x0AnnotationViewerPanel + soundPlayer.getElapsedTime() / scaleFactor /1000;
+					g.setColor(Color.LIGHT_GRAY);
+					g.drawLine(xTime, 0, xTime, 300);
 				}
-				
 			};
 			
 			annotationViewerPanel.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent e) {
 //					super.mouseClicked(e);
+					if(e.getClickCount() < 2) {
+						return;
+					}
 					Point point = e.getPoint();
-					soundPlayer.setPlayPoint((long)(point.x * 4 * 1000));
-//					System.err.println("x:" + point.x + "," + (long)(point.x * 4));
-//					System.err.println("x:" + point.x + "," + (long)(point.x * 4.0f / soundPlayer.getSoundLength()));
+					soundPlayer.setPlayPoint((long)(point.x * scaleFactor * 1000));
 				}
 			});
 		}
@@ -155,13 +151,38 @@ public class AnnotationGlobalViewer extends JPanel {
 		if(namePanel == null){
 			namePanel = new JPanel(){
 				private static final long serialVersionUID = 1L;
+				private final int fontHeight = getFontMetrics(getFont()).getHeight();
 
 				public void paintComponent(Graphics g) {
 					int i = 0;
-					for(String name: discusserNames){
-						g.setColor(Color.black);
-						g.drawString(name, 4, i*25+getFontMetrics(getFont()).getHeight());
-						i++;
+					switch (targetSelector.getSelectedIndex()){
+					case 0:
+						for(String name: discusserNames){
+							g.setColor(Color.black);
+							g.drawString(name, x0NamePanel, y0NamePanel + i * itemHeight + fontHeight);
+							i++;
+						}
+						break;
+					case 1:
+						for(String type: types){
+							g.setColor(Color.black);
+							g.drawString(type, x0NamePanel, y0NamePanel + i * itemHeight + fontHeight);
+							i++;
+						}
+						break;
+					case 2:
+						for(String commenterName: commenterNames){
+							g.setColor(Color.black);
+							g.drawString(commenterName, x0NamePanel, y0NamePanel + i * itemHeight + fontHeight);
+							i++;
+						}
+						break;
+					default:
+						for(String name: discusserNames){
+							g.setColor(Color.black);
+							g.drawString(name, x0NamePanel, y0NamePanel + i * itemHeight + fontHeight);
+							i++;
+						}
 					}
 				}
 			};
@@ -171,36 +192,32 @@ public class AnnotationGlobalViewer extends JPanel {
 		return namePanel;
 	}
 	
-    class UserComparator implements Comparator<User> {  
-        public int compare(User arg0, User arg1) {
-        	return arg0.getName().compareTo(arg1.getName());
-        }
-    }  
-
-    
-    class CommentTypeComparator implements Comparator<CommentType> {  
-        public int compare(CommentType arg0, CommentType arg1) {
-        	return arg0.getType().compareTo(arg1.getType());
-        }
-    }  
-
     
 	public void updatePanel(){
 		discusserNames.clear();
 		for(User discusser: discussers){
-			discusserNames.add(discusser.getName());
+			String name = discusser.getName();
+			if(!discusserNames.contains(name)){
+				discusserNames.add(name);
+			}
 		}
-		Collections.sort(discusserNames);
+//		Collections.sort(discusserNames);
 
 		types.clear();
 		for(CommentType commentType: commentTypes){
-			types.add(commentType.getType());
+			String type = commentType.getType();
+			if(!types.contains(type)){
+				types.add(type);
+			}
 		}
-		Collections.sort(types);
+//		Collections.sort(types);
 		
 		commenterNames.clear();
 		for(Comment comment : commentList){
-			commenterNames.add(comment.getCommenter().getName());
+			String name = comment.getCommenter().getName();
+			if(!commenterNames.contains(name)){
+				commenterNames.add(comment.getCommenter().getName());
+			}
 		}
 		Collections.sort(commenterNames);
 		repaint();
@@ -211,20 +228,5 @@ public class AnnotationGlobalViewer extends JPanel {
 		super.paintComponent(g);
 		displayPanel.repaint();
 		namePanel.repaint();
-	}
-	
-	
-	public void drawTime(int time){
-		revalidate();
-		currentTime = time;
-		Graphics g = getGraphics();
-		g.clearRect(0, 125, 100, 5);
-		g.fillRect(currentTime/1000/2, 125, 5, 5);
-	}
-
-	public void drawTime(int time, boolean flag){
-		this.flag = flag;
-		currentTime = time;
-		repaint();
 	}
 }
