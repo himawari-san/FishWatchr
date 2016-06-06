e495cdf892e7c82310701e712991726865a8fe4a
himawari-san <himawari@ninjal.ac.jp>
2016-06-06 13:27:23 +0900

    prepare for the next release

--- src/org/teachothers/fishwatchr/MainFrame.java
+++ src/org/teachothers/fishwatchr/MainFrame.java
@@ -89,7 +89,7 @@ import org.xml.sax.SAXException;
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private static final String VERSION = "Ver.0.9.5.1 [20160517]";
+	private static final String VERSION = "Ver.0.9.5.2 [20160606]";
 	private static final String COPYRIGHT = "Copyright(c) 2014-2016 Masaya YAMAGUCHI";
 	private static final int TASK_INTERVAL = 250;
 	private static final int THRESHOLD_CLICK_INTERVAL = 800; // ms
