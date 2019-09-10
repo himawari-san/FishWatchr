4c0a33ccf8dcaff0a909f99be4448e3fb59e0749
himawari-san <himawari@ninjal.ac.jp>
2019-09-10 16:10:25 +0900

    refine Util.getCurrentDir()

--- src/org/teachothers/fishwatchr/Util.java
+++ src/org/teachothers/fishwatchr/Util.java
@@ -17,8 +17,6 @@
 
 package org.teachothers.fishwatchr;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.StringWriter;
 
 import javax.xml.transform.OutputKeys;
@@ -44,12 +42,7 @@ public class Util {
 	}
 	
 	public static String getCurrentDir(){
-		try {
-			return new File(new File(System.getProperty("java.class.path")).getCanonicalPath()).getParent(); //$NON-NLS-1$
-		} catch (IOException e) {
-			System.err.println("Warning(Util.getCurrentDir: can't get the current directory."); //$NON-NLS-1$
-			return ""; //$NON-NLS-1$
-		}
+		return System.getProperty("user.dir"); // the directory where fishwatchr.jar is placed
 	}
 	
 
