320efcc027e0a2df723ef0eb8b4ef0cc2c09f650
himawari-san <himawari@ninjal.ac.jp>
2021-07-12 11:00:14 +0900

    Update the year in the license

--- src/org/teachothers/fishwatchr/SoundPanel.java
+++ src/org/teachothers/fishwatchr/SoundPanel.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014-2019 Masaya YAMAGUCHI
+    Copyright (C) 2014-2021 Masaya YAMAGUCHI
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
