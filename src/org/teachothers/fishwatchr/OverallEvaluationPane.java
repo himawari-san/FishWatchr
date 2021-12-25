8fcca8364605042aea2453cc3cdc179b502bc00d
himawari-san <himawari@ninjal.ac.jp>
2021-12-25 11:53:39 +0900

    Make evaluation scale variable

--- src/org/teachothers/fishwatchr/OverallEvaluationPane.java
+++ src/org/teachothers/fishwatchr/OverallEvaluationPane.java
@@ -3,11 +3,16 @@ package org.teachothers.fishwatchr;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -15,9 +20,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 
 
@@ -38,6 +41,8 @@ public class OverallEvaluationPane extends JOptionPane {
 
 	
 	private void ginit(){
+		setOptions(new Object[0]); // remove the default OK button
+
 		int nMainTab = 0;
 		JTabbedPane mainTabPane = new JTabbedPane();
 
@@ -58,20 +63,32 @@ public class OverallEvaluationPane extends JOptionPane {
 		resultTabPane.setTitleAt(nResultTabs++, "Summary");
 		
 		setMessage(mainTabPane);
+		setPreferredSize(new Dimension(600, 800));
 	}
 
 	class InputPanel extends JPanel {
 		private static final long serialVersionUID = 1L;
+		private final Integer[] EVALUATION_SCALES = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+		private static final int EVALUATION_SCALE_POINT = 3; // 5-points-scale
+		
+		private ArrayList<NPointScaleComboBox> evalComboBoxes = new ArrayList<NPointScaleComboBox>();
+		private JComboBox<Integer> evalScaleCB = new JComboBox<Integer>(EVALUATION_SCALES);
 
 		public InputPanel() {
 			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			
 			JPanel optionPanel = new JPanel();
 			JLabel optionLabel = new JLabel("評価方法");
-			JComboBox<String> evalMethodCB = new JComboBox<String>(new String[] {"自由記述", "5段階"});
+			evalScaleCB.setSelectedIndex(EVALUATION_SCALE_POINT);
+			evalScaleCB.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					updateComboBoxes();
+				}
+			});
 			optionPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 			optionPanel.add(optionLabel);
-			optionPanel.add(evalMethodCB);
+			optionPanel.add(evalScaleCB);
 			
 			JPanel evalLabelPanel = new JPanel();
 			JLabel evalLabel = new JLabel("評価項目");
@@ -82,16 +99,22 @@ public class OverallEvaluationPane extends JOptionPane {
 			int nItemsDiscussor = discussers.size();
 			int nItems = nItemsCommentType > nItemsDiscussor ? nItemsCommentType : nItemsDiscussor;
 
-			JComponent[][] evalItems = new JComponent[nItems][5];
+			JComponent[][] evalItems = new JComponent[nItems][7];
 			for(int i = 0; i < nItemsCommentType; i++) {
 				evalItems[i][0] = new JLabel(commentTypes.get(i).getType()); 
-				evalItems[i][1] = new JTextField();
-				evalItems[i][2] = new JLabel();
+				evalItems[i][1] = new NPointScaleComboBox((Integer)evalScaleCB.getSelectedItem());
+				evalItems[i][2] = new CommentButton("コメント");
+				evalItems[i][3] = new JLabel();
+				
+				evalComboBoxes.add((NPointScaleComboBox)evalItems[i][1]);
 			}
 			
 			for(int i = 0; i < nItemsDiscussor; i++) {
-				evalItems[i][3] = new JLabel(discussers.get(i).getName()); 
-				evalItems[i][4] = new JTextField();
+				evalItems[i][4] = new JLabel(discussers.get(i).getName()); 
+				evalItems[i][5] = new NPointScaleComboBox((Integer)evalScaleCB.getSelectedItem());
+				evalItems[i][6] = new CommentButton("コメント");
+
+				evalComboBoxes.add((NPointScaleComboBox)evalItems[i][5]);
 			}
 			
 			GroupLayout gl2 = Util.getGroupLayout(evalItems, evalFormPanel);
@@ -99,16 +122,27 @@ public class OverallEvaluationPane extends JOptionPane {
 
 			JPanel commentLabelPanel = new JPanel();
 			commentLabelPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-			commentLabelPanel.add(new JLabel("コメント"));
+			commentLabelPanel.add(new JLabel("総合コメント"));
 			JScrollPane commentScrollPane = new JScrollPane();
 			commentScrollPane.setViewportView(new JTextArea(10, 30));
+			
+			JPanel buttonPanel = new JPanel();
+			buttonPanel.add(new JButton("保存"));
 			
 			add(optionPanel);
 			add(evalLabelPanel);
 			add(evalFormPanel);
 			add(commentLabelPanel);
 			add(commentScrollPane);
+			add(buttonPanel);
 		}
+		
+		public void updateComboBoxes() {
+			evalComboBoxes.forEach((comboBox)->{
+				comboBox.setValues((Integer)evalScaleCB.getSelectedItem());
+			});
+		}
+		
 	}
 
 	
@@ -123,37 +157,29 @@ public class OverallEvaluationPane extends JOptionPane {
 			evalLabelPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 			evalLabelPanel.add(evalLabel);
 			JPanel evalFormPanel = new JPanel();
-//			evalFormPanel.setPreferredSize(new Dimension(Short.MAX_VALUE, 100));
 			int nItemsCommentType = commentTypes.size();
 			int nItemsDiscussor = discussers.size();
 			int nItems = nItemsCommentType > nItemsDiscussor ? nItemsCommentType : nItemsDiscussor;
 			
-//			JPanel targetsPanel = new JPanel();
-//			targetsPanel.setLayout(new BoxLayout(targetsPanel, BoxLayout.X_AXIS));
-//
-//			JPanel target1Panel = new JPanel();
-//			target1Panel.setLayout(new BoxLayout(target1Panel, BoxLayout.Y_AXIS));
-//			for(int i = 0; i < nItemsCommentType; i++) {
-//				String item = commentTypes.get(i).getType();
-//				target1Panel.add(new JLabel(item)); 
-//			}
-			
-			JComponent[][] evalItems = new JComponent[nItems][5];
+			Random r = new Random();
+			JComponent[][] evalItems = new JComponent[nItems][7];
 			for(int i = 0; i < nItemsCommentType; i++) {
 				String item = commentTypes.get(i).getType();
 				evalItems[i][0] = new JLabel(item); 
 				evalItems[i][0].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][0].getMaximumSize().height));
 				evalItems[i][1] = new JLabel(evaluation.getTarget1(item));
 				evalItems[i][1].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][1].getMaximumSize().height));
-				evalItems[i][2] = new JLabel();
+				evalItems[i][2] = new CommentButton("コメント");
+				evalItems[i][3] = new JLabel();
 			}
 			
 			for(int i = 0; i < nItemsDiscussor; i++) {
 				String item = discussers.get(i).getName();
-				evalItems[i][3] = new JLabel(item);
-				evalItems[i][3].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][3].getMaximumSize().height));
-				evalItems[i][4] = new JLabel(evaluation.getTarget2(item));
-				evalItems[i][4].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][4].getMaximumSize().height));
+				evalItems[i][4] = new JLabel(item);
+				evalItems[i][4].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][3].getMaximumSize().height));
+				evalItems[i][5] = new JLabel(String.valueOf(r.nextInt(5)+1));
+				evalItems[i][5].setMaximumSize(new Dimension(Short.MAX_VALUE, evalItems[i][4].getMaximumSize().height));
+				evalItems[i][6] = new CommentButton("コメント");
 			}
 			
 			GroupLayout gl2 = Util.getGroupLayout(evalItems, evalFormPanel);
@@ -177,6 +203,55 @@ public class OverallEvaluationPane extends JOptionPane {
 			add(evalFormPanel);
 			add(dummy);
 		}
+	}
+	
+	class CommentButton extends JButton {
+
+		private static final long serialVersionUID = 1L;
+		private static final int TEXT_AREA_ROWS = 8;
+		private static final int TEXT_AREA_COLUMNS = 36;
+		private JTextArea textArea = new JTextArea(TEXT_AREA_ROWS, TEXT_AREA_COLUMNS);
+
+		public CommentButton(String text) {
+			super(text);
+			addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					JPanel p = new JPanel();
+					p.setLayout(new BorderLayout());
+					p.add(new JScrollPane(textArea), BorderLayout.CENTER);
+					JOptionPane.showMessageDialog(null, p);
+				}
+			});
+		}
+		
+		public String getComment() {
+			return textArea.getText();
+		}
+	}
+	
+	
+	class NPointScaleComboBox extends JComboBox<String> {
+
+		private static final long serialVersionUID = 1L;
+		private static final String UNENTERED_LABEL = "(未入力)";
+
+		public NPointScaleComboBox(int n) {
+			setValues(n);
+		}
+		
+		public void setValues(int n) {
+			DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>)getModel();
+			ArrayList<String> scale = new ArrayList<String>();
+			for(int i = 1; i <= n; i++) {
+				scale.add(Integer.toString(i));
+			}
+			model.removeAllElements();
+			model.addElement(UNENTERED_LABEL);
+			model.addAll(scale);
+		}
+
 	}
 
 }
